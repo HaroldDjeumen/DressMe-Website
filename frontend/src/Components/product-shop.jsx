9fc940da772924e50b6e37c4cd1eace2ca5cd87e
harold <harolddjeumen@gmail.com>
2025-01-02 14:12:36 +0200

    update

--- frontend/src/Components/product-shop.jsx
+++ frontend/src/Components/product-shop.jsx
@@ -152,7 +152,7 @@ const Products = () => {
                 <h2 className="text-3xl font-bold text-[#3a403a] text-center mb-8">
                     {category === "All" ? "All Products" : category}
                 </h2>
-                <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+<div className="grid gap-6 grid-cols-2 md:grid-cols-3">
                     {filteredProducts.map((feature, index) => (
                         <div
                             key={`product-${index}`}
